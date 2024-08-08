e12f9e158ba4bbf1026be0f22234a84d8293c942
A <2353130@cognizant.com>
2024-08-08 13:46:31 +0530

    added interview questions

---
+++ src/main/java/com/java8/streamsExample/interviewQuestions/IncrementEmpSal.java
@@ -0,0 +1,22 @@
+package com.java8.streamsExample.interviewQuestions;
+
+import com.java8.streamsExample.core.Employee;
+import com.java8.streamsExample.core.LoadEmployees;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class IncrementEmpSal {
+
+    public static void main(String[] args) {
+        List<Employee> input = LoadEmployees.getEmployeeDate();
+
+        List<Employee> collect = input.stream().filter(e -> e.getAge() > 32).map(e->{
+            e.setSalary(e.getSalary() + e.getSalary()*20/100);
+            return e;
+        }).sorted(Comparator.comparing(Employee::getSalary).reversed()).collect(Collectors.toList());
+        collect.forEach(System.out::println);
+    }
+}
--- src/main/java/com/java8/streamsExample/interviewQuestions/Test.java
+++ src/main/java/com/java8/streamsExample/interviewQuestions/Test.java
@@ -2,18 +2,13 @@ package com.java8.streamsExample.interviewQuestions;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Test {
     public static void main(String[] args) {
-        List<List<String>> listOfLists = Arrays.asList(
-                Arrays.asList("cat", "dog", "elephant"),
-                Arrays.asList("ant", "bat", "beetle"),
-                Arrays.asList("fish", "shark")
-        );
-
-        List<String> result = listOfLists.stream().flatMap(List::stream).filter(e -> e.length()>4).collect(Collectors.toList());
-        result.forEach(System.out::println);
+        LinkedList<String> input = new LinkedList<>();
+        input.add("");
     }
 }
